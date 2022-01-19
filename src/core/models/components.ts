d64a71dca0bf85b96f54ec4f11092f368a1209d5
Balazs E. Pataki <pataki@gmail.com>
2022-01-19 12:03:45 +0100

    Use calculated previewDefaultProps instead of hard coded DEFAULT_PROPS
    Wherever DEFAULT_PROPS has been used now previewDefaultProps is used, which
    is derived from componentDefs.

--- src/core/models/components.ts
+++ src/core/models/components.ts
@@ -1,12 +1,11 @@
 import { createModel } from '@rematch/core'
 import produce from 'immer'
-import { DEFAULT_PROPS } from '~utils/defaultProps'
 import templates, { TemplateType } from '~templates'
 import { generateId } from '~utils/generateId'
 import { duplicateComponent, deleteComponent } from '~utils/recursive'
 import omit from 'lodash/omit'
 import {IComponent, IComponents} from "~componentDefsTypes";
-import {ComponentType} from "~componentDefs";
+import {ComponentType, previewDefaultProps} from "~componentDefs";
 
 export type ComponentsState = {
   components: IComponents
@@ -55,7 +54,7 @@ const components = createModel({
     resetProps(state: ComponentsState, componentId: string): ComponentsState {
       return produce(state, (draftState: ComponentsState) => {
         const component = draftState.components[componentId]
-        const { form, ...defaultProps } = DEFAULT_PROPS[component.type] || {}
+        const { form, ...defaultProps } = previewDefaultProps[component.type] || {}
 
         draftState.components[componentId].props = defaultProps || {}
       })
@@ -160,7 +159,7 @@ const components = createModel({
     ): ComponentsState {
       return produce(state, (draftState: ComponentsState) => {
         const id = payload.testId || generateId()
-        const { form, ...defaultProps } = DEFAULT_PROPS[payload.type] || {}
+        const { form, ...defaultProps } = previewDefaultProps[payload.type] || {}
         draftState.selectedId = id
         draftState.components[payload.parentName].children.push(id)
         draftState.components[id] = {
